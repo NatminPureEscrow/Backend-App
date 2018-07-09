b728129a29608a3e4931c86c1ecd837ed8b725bc
Natmin Pure Escrow <39843150+natminpe@users.noreply.github.com>
2018-07-09 22:07:34 +1000

    Add files via upload

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,7 @@
+var NatminApplication = artifacts.require("./NatminApplication.sol");
+var TokenAddress = "0x8053f13FdA4C488e8BB8c9CF7b97e7B27A74293B";
+
+module.exports = function(deployer) {
+  
+  
+};
